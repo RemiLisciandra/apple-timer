86c14f5fd71e909682b219bbd8441ff2feaf4974
RemiLisciandra <rlisciandra@gmail.com>
2024-08-16 14:19:53 +0200

    Timer prêt à être utilisé

--- app/hooks/useTimerStore.jsx
+++ app/hooks/useTimerStore.jsx
@@ -15,9 +15,65 @@ const useTimerStore = () => {
     setTimers((prevTimers) => [...prevTimers, newTimer]);
   };
 
+  const removeTimer = (id) => {
+    setTimers((prevTimers) => prevTimers.filter((pt) => pt.id !== id));
+  };
+
+  const toggleRunning = (id) => {
+    setTimers((prevTimers) => {
+      const timerIndex = prevTimers.findIndex((timer) => timer.id === id);
+
+      if (timerIndex === -1) return prevTimers;
+
+      const timerToUpdate = prevTimers[timerIndex];
+
+      if (timerToUpdate.timeLeft === 0) {
+        const updatedTimer = {
+          ...timerToUpdate,
+          timeLeft: timerToUpdate.duration,
+          isRunning: false,
+        };
+
+        return [
+          ...prevTimers.slice(0, timerIndex),
+          updatedTimer,
+          ...prevTimers.slice(timerIndex + 1),
+        ];
+      }
+
+      if (!timerToUpdate.isRunning) {
+        const updatedTimer = {
+          ...timerToUpdate,
+          isRunning: true,
+          endAt: Date.now() + timerToUpdate.timeLeft,
+        };
+        return [
+          ...prevTimers.slice(0, timerIndex),
+          updatedTimer,
+          ...prevTimers.slice(timerIndex + 1),
+        ];
+      }
+
+      if (timerToUpdate.isRunning) {
+        const updatedTimer = {
+          ...timerToUpdate,
+          isRunning: false,
+          endAt: timerToUpdate.timeLeft - Date.now(),
+        };
+        return [
+          ...prevTimers.slice(0, timerIndex),
+          updatedTimer,
+          ...prevTimers.slice(timerIndex + 1),
+        ];
+      }
+    });
+  };
+
   return {
     timers,
     addTimer,
+    removeTimer,
+    toggleRunning,
   };
 };
 
